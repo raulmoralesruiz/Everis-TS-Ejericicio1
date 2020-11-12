3e82dc8f96475f532423aa1aa7079afbf9bce142
raulmoralesruiz <raulmdaw@gmail.com>
2020-11-12 14:05:36 +0100

    Se añaden comentarios

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 // Tipad
 // Para determinar el tipo de los parámetros de la función, debes dar por hecho que los tipos van a ser siempre los de las dos variables que se añaden en la llamada de la linea 20.
 
+// se añade tipado a las variables.
 let a: number;
 let b: string;
 let c: boolean | number;
@@ -13,6 +14,7 @@ if (Math.random() >= 0.5) {
   c = 100;
 }
 
+// se añade tipado a los parámetros y a la función
 function d(param1: number, param2: number | boolean): number | string {
   if (typeof param2 === "number") {
     return param1 + param2;
